3c7002fd0d679c4e31f7d7e9d588533f258b48bc
lango <heracrito@gmail.com>
2024-09-26 21:09:54 -0300

    css

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 # Create your views here.
 
 def index(requests):
+    contacts = Contact.objects.all()
+
+    context = {
+        'contacts': contacts,
+    }
+
     return render(
         requests,
         'contact/index.html',
+        context,
     )
